d32a094e46284da3b9833db509dd0d77d1bd6106
Edd Inglis <edd.inglis@gmail.com>
2021-05-02 15:54:44 +0100

    change of plan on the control syntax, so revert the 'new' version

--- dimmer/control.ino
+++ dimmer/control.ino
@@ -49,125 +49,16 @@ static uint8_t buf[num_outputs * 2];
 static const size_t buf_invalid = ~0;
 static size_t buf_used = buf_invalid;
 
-enum parse_state {
-    ps_idle,
-    ps_collecting_level,
-    ps_collecting_rate,
-};
-static parse_state ps = ps_idle;
-static int ps_channel = 0;
-static int ps_level = 0;
-static int ps_rate = 0;
-
-void handle_update( int channel, int level, int rate = -1 )
-{
-    if (channel < 0 || channel >= num_channels)
-        return;
-
-    if (level > phase_max)
-        level = phase_max;
-    if (rate > 8)
-        rate = 8;
-
-    Serial.print("update channel ");
-    Serial.print((char)(channel + 'A'));
-    Serial.print(": level ");
-    Serial.print(level);
-    Serial.print(", rate ");
-    if (rate >= 0)
-        Serial.print(rate);
-    else
-        Serial.print("unchanged");
-    Serial.println(".");
-
-    output_target[channel] = level;
-    if (rate >= 0)
-        output_rate[channel] = rate;
-}
-
 void poll_serial()
 {
     while (Serial.available())
     {
         const int ch = Serial.read();
-    if(0){
         if (ch == (char)boot_char)
         {
             report_id();
-            ps = ps_idle;
         }
-        else if (ch >= 'A' && ch < 'A' + num_channels)
-        {
-            if (ps == ps_idle)
-            {
-                ps_channel = ch - 'A';
-                ps_level = 0;
-                ps_rate = 0;
-
-                //Serial.println("got channel");
-                ps = ps_collecting_level;
-            }
-            else
-            {
-                Serial.println("error 1");
-                ps = ps_idle;
-            }
-        }
-        else if (ch >= '0' && ch <= '9')
-        {
-            if (ps == ps_collecting_level)
-            {
-                ps_level = ps_level * 10 + (ch - '0');
-            }
-            else if (ps == ps_collecting_rate)
-            {
-                ps_rate = ps_rate * 10 + (ch - '0');
-            }
-            else
-            {
-                //Serial.println("error 2");
-                ps = ps_idle;
-            }
-        }
-        else if (ch == ':')
-        {
-            if (ps == ps_collecting_level)
-            {
-                //Serial.print("got level ");
-                //Serial.println(ps_level);
-                ps = ps_collecting_rate;
-            }
-            else
-            {
-                Serial.println("error 3");
-                ps = ps_idle;
-            }
-        }
-        else if (ch == ' ' || ch == ';' || ch == '\n' || ch == '\r')
-        {
-            if (ps == ps_collecting_level)
-            {
-                //Serial.print("got level ");
-                //Serial.println(ps_level);
-                handle_update( ps_channel, ps_level );
-            }
-            else if (ps == ps_collecting_rate)
-            {
-                //Serial.print("got rate ");
-                //Serial.println(ps_rate);
-                handle_update( ps_channel, ps_level, ps_rate );
-            }
-            // it might be an error, it might not; it makes no odds.
-            ps = ps_idle;
-        }
-        else
-        {
-            // siliently ignore
-            ps = ps_idle;
-        }
-    } else {
-
-         if (ch == '{')
+         else if (ch == '{')
          {
              buf_used = 0;
          }
@@ -188,7 +79,5 @@ void poll_serial()
          {
              buf_used = buf_invalid;
          }
-
-    }
     }
 }
